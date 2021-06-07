e814db1f01a9efe493a28812895f70898e835e25
indra4837 <indra4837@gmail.com>
2021-06-07 23:39:41 +0800

    disable back button on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,227 +43,230 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              HeaderLogo(),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.15,
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width,
-                  maxHeight: 100,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.1,
-                        child: Stack(
-                          children: [
-                            Align(
-                              alignment: Alignment(0, 0),
-                              child: Container(
-                                width: 50,
-                                height: 100,
-                                constraints: BoxConstraints(
-                                  maxWidth: 45,
-                                  maxHeight: 25,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: Color(0xFFD5E5E2),
-                                  borderRadius: BorderRadius.circular(29),
-                                  shape: BoxShape.rectangle,
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment(0.14, 0.05),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'All',
-                                  textAlign: TextAlign.start,
-                                  style: GoogleFonts.alegreyaSans(
-                                    textStyle: TextStyle(
-                                      color: Color(0xFF5B7D76),
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                    ),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                HeaderLogo(),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width,
+                    maxHeight: 100,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.1,
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment(0, 0),
+                                child: Container(
+                                  width: 50,
+                                  height: 100,
+                                  constraints: BoxConstraints(
+                                    maxWidth: 45,
+                                    maxHeight: 25,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFD5E5E2),
+                                    borderRadius: BorderRadius.circular(29),
+                                    shape: BoxShape.rectangle,
                                   ),
                                 ),
                               ),
-                            )
-                          ],
+                              Align(
+                                alignment: Alignment(0.14, 0.05),
+                                child: TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'All',
+                                    textAlign: TextAlign.start,
+                                    style: GoogleFonts.alegreyaSans(
+                                      textStyle: TextStyle(
+                                        color: Color(0xFF5B7D76),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment(0.14, 0.05),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Courts',
-                            textAlign: TextAlign.start,
-                            style: GoogleFonts.alegreyaSans(
-                              textStyle: TextStyle(
-                                color: Color(0xFF666462),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                      Align(
+                        alignment: Alignment(0.14, 0.05),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Courts',
+                              textAlign: TextAlign.start,
+                              style: GoogleFonts.alegreyaSans(
+                                textStyle: TextStyle(
+                                  color: Color(0xFF666462),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment(0.14, 0.05),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Pitch',
-                            textAlign: TextAlign.start,
-                            style: GoogleFonts.alegreyaSans(
-                              textStyle: TextStyle(
-                                color: Color(0xFF666462),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                      Align(
+                        alignment: Alignment(0.14, 0.05),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Pitch',
+                              textAlign: TextAlign.start,
+                              style: GoogleFonts.alegreyaSans(
+                                textStyle: TextStyle(
+                                  color: Color(0xFF666462),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment(0.14, 0.05),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Match',
-                            textAlign: TextAlign.start,
-                            style: GoogleFonts.alegreyaSans(
-                              textStyle: TextStyle(
-                                color: Color(0xFF666462),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                      Align(
+                        alignment: Alignment(0.14, 0.05),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Match',
+                              textAlign: TextAlign.start,
+                              style: GoogleFonts.alegreyaSans(
+                                textStyle: TextStyle(
+                                  color: Color(0xFF666462),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment(0.14, 0.05),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Futsal',
-                            textAlign: TextAlign.start,
-                            style: GoogleFonts.alegreyaSans(
-                              textStyle: TextStyle(
-                                color: Color(0xFF666462),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                      Align(
+                        alignment: Alignment(0.14, 0.05),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Futsal',
+                              textAlign: TextAlign.start,
+                              style: GoogleFonts.alegreyaSans(
+                                textStyle: TextStyle(
+                                  color: Color(0xFF666462),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment(0, 0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Text(
-                      'Featured Games',
-                      textAlign: TextAlign.start,
-                      style: GoogleFonts.alegreyaSans(
-                        textStyle: TextStyle(
-                          color: Color(0xFF666462),
-                          fontSize: 23,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 1,
-                height: MediaQuery.of(context).size.height * 0.3,
-                child: CarouselPage(),
-              ),
-              // Carousel(),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  alignment: Alignment(-1, 0),
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Text(
-                      'Nearby Courts',
-                      style: GoogleFonts.openSans(
-                        textStyle: TextStyle(
-                          color: Color(0xFF666462),
-                          fontSize: 23,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: smallIcons
-                      .map(
-                        (entry) => SmallFeatures(
-                          getImageURL(entry['image']),
-                          entry['rating'],
-                          entry['courtName'],
-                          entry['time'],
-                          entry['location'],
                         ),
                       )
-                      .toList(),
+                    ],
+                  ),
                 ),
-              )
-            ],
+                Align(
+                  alignment: Alignment(0, 0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: Text(
+                        'Featured Games',
+                        textAlign: TextAlign.start,
+                        style: GoogleFonts.alegreyaSans(
+                          textStyle: TextStyle(
+                            color: Color(0xFF666462),
+                            fontSize: 23,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: CarouselPage(),
+                ),
+                // Carousel(),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    alignment: Alignment(-1, 0),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: Text(
+                        'Nearby Courts',
+                        style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            color: Color(0xFF666462),
+                            fontSize: 23,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: smallIcons
+                        .map(
+                          (entry) => SmallFeatures(
+                            getImageURL(entry['image']),
+                            entry['rating'],
+                            entry['courtName'],
+                            entry['time'],
+                            entry['location'],
+                          ),
+                        )
+                        .toList(),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
